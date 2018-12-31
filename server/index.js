afde8f007ef5538b6db20925ab67e6233968251b
Jeremy Meyer <jeremythedev@gmail.com>
2018-12-31 12:29:53 -0800

    Saving progress

--- server/index.js
+++ server/index.js
@@ -1,52 +1,64 @@
-var express = require('express');
-var fs = require('fs');
-var mongoose = require('mongoose');
-var passport = require('passport');
+var koa = require('koa');
 var secrets = require('./config/secrets');
-var webpack = require('webpack');
-var config = require('../webpack/webpack.config.dev.js');
-var app = express();
-var compiler = webpack(config);
+var app = koa();
 
-// Find the appropriate database to connect to, default to localhost if not found.
-var connect = function() {
-  mongoose.connect(secrets.db, function(err, res) {
-    if(err) {
-      console.log('Error connecting to: ' + secrets.db + '. ' + err);
-    }else {
-      console.log('Succeeded connected to: ' + secrets.db);
-    }
-  });
-};
-connect();
+const webpackDevServer = require('koa-webpack-dev'); // config.output.publicPath
+app.use(webpackDevServer(settings));
 
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
-
-// Bootstrap models
-fs.readdirSync(__dirname + '/models').forEach(function(file) {
-  if(~file.indexOf('.js')) require(__dirname + '/models/' + file);
+app.use(function * () {
+  this.body = 'Hello World';
 });
 
-var isDev = process.env.NODE_ENV === 'development';
+app.listen(3000);
 
-if (isDev) {
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }));
+// var express = require('express');
+// var fs = require('fs');
+// var mongoose = require('mongoose');
+// var passport = require('passport');
+// var webpack = require('webpack');
+// var config = require('../webpack/webpack.config.dev.js');
+// var app = express();
+// var compiler = webpack(config);
 
-  app.use(require('webpack-hot-middleware')(compiler));
-}
+// // Find the appropriate database to connect to, default to localhost if not found.
+// var connect = function() {
+//   mongoose.connect(secrets.db, function(err, res) {
+//     if(err) {
+//       console.log('Error connecting to: ' + secrets.db + '. ' + err);
+//     }else {
+//       console.log('Succeeded connected to: ' + secrets.db);
+//     }
+//   });
+// };
+// connect();
+
+// mongoose.connection.on('error', console.log);
+// mongoose.connection.on('disconnected', connect);
+
+// // Bootstrap models
+// fs.readdirSync(__dirname + '/models').forEach(function(file) {
+//   if(~file.indexOf('.js')) require(__dirname + '/models/' + file);
+// });
+
+// var isDev = process.env.NODE_ENV === 'development';
+
+// if (isDev) {
+//   app.use(require('webpack-dev-middleware')(compiler, {
+//     noInfo: true,
+//     publicPath: config.output.publicPath
+//   }));
+
+//   app.use(require('webpack-hot-middleware')(compiler));
+// }
 
 
-// Bootstrap passport config
-require('./config/passport')(app, passport);
+// // Bootstrap passport config
+// require('./config/passport')(app, passport);
 
-// Bootstrap application settings
-require('./config/express')(app, passport);
+// // Bootstrap application settings
+// require('./config/express')(app, passport);
 
-// Bootstrap routes
-require('./config/routes')(app, passport);
+// // Bootstrap routes
+// require('./config/routes')(app, passport);
 
-app.listen(app.get('port'));
+// app.listen(app.get('port'));
